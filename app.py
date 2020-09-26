c80fb1acc641381e135b5d372771f4e9e75cdddd
pgunaseelan <71909165+pgunaseelan@users.noreply.github.com>
2020-09-26 00:40:43 -0400

    Offers Added

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import pymongo
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return 'Rans is a good person'
-
-@app.route('/json', methods=['GET'])
-def send_json():
-    return jsonify({'texts': "hello"})
-
-
 client = pymongo.MongoClient(
    "mongodb+srv://Avatars:QrQnDDetR8cceWAS@cluster0.g15s2.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.WaveDirectBackend
 users = db.Users
 print(users.find_one())
+
+@app.route('/offers', methods=['GET'])
+def offers():
+    packages = db.Packages
+    return jsonify(list(packages.find({})))
+
+
 
